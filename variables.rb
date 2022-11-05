a5adca22d443e1a47bdc67cfd17b99f180bc15bb
Granhn <fuenzaladeveloper@gmail.com>
2022-11-05 17:59:08 -0300

    Variableshsh y viendo strings

---
+++ variables.rb
@@ -0,0 +1,21 @@
+character_name ="Diego"
+character_age = "22"
+puts ("There once was a man named "+ character_name + ".")
+puts ("He was "+ character_age+ " years old.")
+puts ("He really liked the name" + character_name)
+puts ("but didn't like being " + character_age)
+
+
+#DATA TYPES ------------------------------------------------------------------------------
+string_type = "Soy una variable string en ruby"
+number_type = 22
+decimal_type = 22.98
+negative_numbers = -22
+boolean_type = true
+no_value = nil #Es una vaiable que no tiene valor
+puts no_value
+no_value = "Value"
+puts no_value
+no_value = true
+puts no_value
+puts "Las variables son de tiado dinamico asi que ojo"
