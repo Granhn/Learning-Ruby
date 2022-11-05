10c638518514863bdca56d431605f29e0625f322
Granhn <fuenzaladeveloper@gmail.com>
2022-11-05 17:39:33 -0300

    Viendo variables

--- holMundo.rb
+++ holMundo.rb
@@ -1,1 +1,7 @@
-puts "hola Diego"
+puts "Puts escribe cosas con un salto de linea al final, mientras que print"    
+print "Print no hace unn salto de linea, mas bien continua exactamente donde termina el print"
+print ". Por ejempolo este es un print escrito bajo otro print"
+
+puts " .   /|"
+puts " .  / |"
+puts " . /  |"
